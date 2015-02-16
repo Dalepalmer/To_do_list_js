f8a38b9a21a27c7cdd7993bb7afe58705c935a93
Dale <student@epicodus.com>
2015-02-16 15:51:53 -0800

    full web functionality to our spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,25 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedTask = $("input#new-task").val();
-    var newTask = {taskName: inputtedTask}
+    var newTask = {taskName: inputtedTask, doneTask: false};
 
     $("ul#undone-tasks").append("<li><span class='undoneTask'>" + newTask.taskName + "</span></li>");
 
-    $(".task").last().click(function() {
+    $(".undoneTask").last().click(function() {
       $("#show-undone-task").show();
-      $("#show-undone-task h2").text(newTask.taskName);
+      // $("#show-undone-task h2").text(newTask.taskName);
       $(".task-name").text(newTask.taskName);
+        newTask = {taskName: inputtedTask, doneTask: true};
+
+      if (newTask.doneTask === true) {
+        $("ul#done-tasks").append("<ul><span class='doneTask'>" + newTask.taskName + "</ul></span>");
+      }
+
+      $(".doneTask").last().click(function() {
+        $(".task-name").remove();
+        $(".doneTask").remove();
+
+      });
     });
   });
 });
